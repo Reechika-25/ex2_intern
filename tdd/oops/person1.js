eabe2c4a6386e56383c3a9098c62025c5e56e6ca
Reechika-25 <rjain@ex2india.com>
2021-02-01 12:42:49 +0530

    added requirement 2 in oops

---
+++ tdd/oops/person1.js
@@ -0,0 +1,30 @@
+export class Person{
+
+    constructor(name,address,age){
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
+}
+
+
+export class PersonCollection{
+
+    constructor(){
+        this.personArray = [];
+    }
+
+    addPerson (name,address,age){
+        this.personArray.push(new Person(name,address,age));
+    }
+
+    printInfo(){
+
+        let out = [];
+        for(let itr = 0;itr<this.personArray.length;itr++)
+            out.push({name: this.personArray[itr].name , address : this.personArray[itr].address, age : this.personArray[itr].age});
+        return out;
+    }
+    
+
+}
