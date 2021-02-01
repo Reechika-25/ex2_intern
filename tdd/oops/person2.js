b5bd679e14f722f98742c2a88277ac3dbebfc6ee
Reechika-25 <rjain@ex2india.com>
2021-02-01 13:35:18 +0530

    added final stage3 of oops

--- tdd/oops/person2.js
+++ tdd/oops/person2.js
@@ -1,76 +1,62 @@
-class StringOp{
-    
-    static toTitleCase(str){
-
-        if(str.length === 0)
-           return str;
-        
-        str = str.toLowerCase();
-        str[0] = str[0] -'a' + 'A';
-        return str;
+export class Person {
+    constructor(name, address, age) {
+      this.name = name;
+      this.address = address;
+      this.age = age;
     }
-}
-
-export class Person{
-
-    constructor(name,address,age){
-        this.name = name;
-        this.address = address;
-        this.age = age;
+  }
+  export class PersonCollection {
+    constructor() {
+      this.personArray = [];
     }
-}
-
-
-export class PersonCollection{
-
-    constructor(){
-        this.personArray = [];
+    addPerson(name, address, age) {
+      this.personArray.push(new Person(name, address, age));
+      return '';
     }
-
-    addPerson (name,address,age){
-        this.personArray.push(new Person(name,address,age));
+    printInfo() {
+      let out = [];
+      for (let itr = 0; itr < this.personArray.length; itr++)
+        out.push({
+          name: this.personArray[itr].name,
+          address: this.personArray[itr].address,
+          age: this.personArray[itr].age
+        });
+      return out;
     }
-
-    printInfo(){
-
-        let out = [];
-        for(let itr = 0;itr<this.personArray.length;itr++)
-            out.push({name: this.personArray[itr].name , address : this.personArray[itr].address, age : this.personArray[itr].age});
-        return out;
+    personsBornAfter1990() {
+      let out = [];
+      for (let itr = 0; itr < this.personArray.length; itr++) {
+        if (this.personArray[itr].age <= 30)
+          out.push({
+            address: this.personArray[itr].address,
+            age: this.personArray[itr].age,
+            name: this.personArray[itr].name
+          });
+      }
+      return out;
     }
-
-    personsBornAFter1990(){
-
-        let out = [];
-        for(let itr = 0;itr< this.personArray.length;itr++){
-            if(this.personArray[itr].age <= 30)
-                out.push(this.personArray[itr]);
-        }
-        return out;
+    personNameStartsWithS() {
+      let out = [];
+      for (let itr = 0; itr < this.personArray.length; itr++) {
+        let str = this.personArray[itr].name;
+        if (str[0] === 'S' || str[0] === 's')
+          out.push({
+            name: this.personArray[itr].name,
+            address: this.personArray[itr].address,
+            age: this.personArray[itr].age
+          });
+      }
+      return out;
     }
-
-    personNameStartsWithS(){
-        let out = [];
-        for(let itr = 0;itr< this.personArray.length;itr++){
-            let str = this.personArray.name;
-            str = StringOp.toTitleCase(str);
-            if(str[0] === 'S')
-              out.push(this.personArray[itr]);
-        }
-
-        return out;
-
+    personLivingInCityNoida() {
+      let out = [];
+      for (let itr = 0; itr < this.personArray.length; itr++) {
+        if (this.personArray[itr].address.contains('Noida'))
+          out.push(this.personArray[itr]);
+      }
+      return out;
     }
-
-    personLivingInCityNoida(){
-
-        let out = [];
-        for(let itr = 0;itr< this.personArray.length;itr++){
-            if(this.personArray[itr].address.contains('Noida'))
-                out.push(this.personArray[itr]);
-        }
-        return out;
-    }
-    
-
-}
+  }
+  white_check_mark
+  eyes
+  
--- tdd/oops/person2.test.js
+++ tdd/oops/person2.test.js
@@ -1,90 +1,77 @@
-import {Person} from "./person"
-import {PersonCollection}  from "./person"
-
-describe("Testing Person functionality",()=>{
-
-    it("does person class exists",()=>{
-
-        let personInstance = new Person();
-        expect(personInstance).toBeDefined();
-    });
- 
-    it("does name address age initialised in constructor",()=>{
-
-        const name = "Reechika";
-        const address = "110A/6,Faridabad"
-        const age = 21;
-
-        let personInstance = new Person(name,address,age);
-        expect(personInstance.name).toBeDefined();
-        expect(personInstance.address).toBeDefined();
-        expect(personInstance.age).toBeDefined();
-    });
-
-
-    it("does personcollection class exists",()=>{
-
-        let colInstance = new PersonCollection();
-        expect(colInstance).toBeDefined();
-        expect(colInstance.personArray).toBeDefined();
-    });
-
-    it("does add person and print info function exits in class",()=>{
-
-        let colInstance = new PersonCollection();
-        expect(colInstance.addPerson()).toBeDefined();
-        expect(colInstance.printInfo()).toBeDefined();
-        expect(typeof colInstance.printInfo()).toBe('object');
-    });
-
-
-    it("does add function works properly ?",()=>{
-
-        let colInstance = new PersonCollection();
-        expect(colInstance.addPerson('sanjay','fbd',18)).toBeDefined();
-    });
-
-
-    it("print function work properly",()=>{
-
-        let colInstance = new PersonCollection();
-        colInstance.addPerson('Reechika','fbd',25);
-        colInstance.addPerson('sanjay','fbd',21);
-        expect(colInstance.printInfo()).toStrictEqual([{name : 'Reechika', address : 'fbd' , age : 25},
-                                                        {name : 'sanjay' , address : 'fbd', age : 21}]);
-    });
-
-
-    it("does person born after 1990 function work properly",()=>{
-
-        let colInstance = new PersonCollection();
-        colInstance.addPerson('Reechika','fbd',25);
-        colInstance.addPerson('sanjay','fbd',21);
-        expect(colInstance.personsBornAfter1990()).toStrictEqual([{name : 'Reechika', address : 'fbd' , age : 25},
-                                                        {name : 'sanjay' , address : 'fbd', age : 21}]);
-    });
-
-    it("does person name starts with s function work properly ",()=>{
-
-        let colInstance = new PersonCollection();
-        colInstance.addPerson('Reechika','fbd',25);
-        colInstance.addPerson('sanjay','fbd',21);
-        expect(colInstance.personNameStartsWithS()).toStrictEqual([{name : 'sanjay' , address : 'fbd', age : 21}]);
-    });
-
-
-    it("does person living in noida function work properly",()=>{
-
-        let colInstance = new PersonCollection();
-        colInstance.addPerson('Reechika','Noida',25);
-        colInstance.addPerson('sanjay','Noida',21);
-        expect(colInstance.printInfo()).toStrictEqual([{name : 'Reechika', address : 'Noida' , age : 25},
-                                                        {name : 'sanjay' , address : 'Noida', age : 21}]);
-    });
-
-
-
-
-
-
+import { Person } from './person2';
+import { PersonCollection } from './person2';
+describe('Testing Person functionality', () => {
+  it('does person class exists', () => {
+    let personInstance = new Person();
+    expect(personInstance).toBeDefined();
+  });
+  it('does name address age initialised in constructor', () => {
+    const name = 'Reechika';
+    const address = '110A/6,Faridabad';
+    const age = 21;
+    let personInstance = new Person(name, address, age);
+    expect(personInstance.name).toBeDefined();
+    expect(personInstance.address).toBeDefined();
+    expect(personInstance.age).toBeDefined();
+  });
+  it('does personcollection class exists', () => {
+    let colInstance = new PersonCollection();
+    expect(colInstance).toBeDefined();
+    expect(colInstance.personArray).toBeDefined();
+  });
+  it('does add person and print info function exits in class', () => {
+    let colInstance = new PersonCollection();
+    expect(colInstance.addPerson()).toBeDefined();
+    expect(colInstance.printInfo()).toBeDefined();
+    expect(typeof colInstance.printInfo()).toBe('object');
+  });
+  it('does add function works properly ?', () => {
+    let colInstance = new PersonCollection();
+    expect(colInstance.addPerson('sanjay', 'fbd', 18)).toBeDefined();
+  });
+  it('print function work properly', () => {
+    let colInstance = new PersonCollection();
+    colInstance.addPerson('Reechika', 'fbd', 25);
+    colInstance.addPerson('sanjay', 'fbd', 21);
+    expect(colInstance.printInfo()).toStrictEqual([
+      { name: 'Reechika', address: 'fbd', age: 25 },
+      { name: 'sanjay', address: 'fbd', age: 21 }
+    ]);
+  });
+  it('does person born after 1990 function work properly', () => {
+    let colInstance = new PersonCollection();
+    colInstance.addPerson('Reechika', 'fbd', 25);
+    colInstance.addPerson('sanjay', 'fbd', 21);
+    expect(colInstance.personsBornAfter1990()).toStrictEqual([
+      { address: 'fbd', age: 25, name: 'Reechika' },
+      { address: 'fbd', age: 21, name: 'sanjay' }
+    ]);
+  });
+  it('does person name starts with s function work properly ', () => {
+    let colInstance = new PersonCollection();
+    colInstance.addPerson('Reechika', 'fbd', 25);
+    colInstance.addPerson('sanjay', 'fbd', 21);
+    expect(colInstance.personNameStartsWithS()).toStrictEqual([
+      { name: 'sanjay', address: 'fbd', age: 21 }
+    ]);
+  });
+  it('does person living in noida function work properly', () => {
+    let colInstance = new PersonCollection();
+    colInstance.addPerson('Reechika', 'Noida', 25);
+    colInstance.addPerson('sanjay', 'Noida', 21);
+    expect(colInstance.printInfo()).toStrictEqual([
+      { name: 'Reechika', address: 'Noida', age: 25 },
+      { name: 'sanjay', address: 'Noida', age: 21 }
+    ]);
+  });
 });
+white_check_mark
+eyes
+raised_hands
+
+
+
+
+
+New
+1:33
