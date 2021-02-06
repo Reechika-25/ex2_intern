ac185d0a94c9bdec226b5aa45485b936cffd5307
Reechika-25 <rjain@ex2india.com>
2021-02-07 00:04:57 +0530

    refactored basic

--- tdd/basic/string.js
+++ tdd/basic/string.js
@@ -4,19 +4,22 @@ export class theString{
         if(typeof arr === 'undefined')
            return "please send input";
 
-        if(typeof arr != 'object') //mistake-- typeof returns a string and also it returns object in case of an array
+        if(typeof arr !== 'object') 
            return "send input as array of strings";
 
-        let out = [];
-
-        for(let itr =0;itr<arr.length;itr++){
-            if(typeof  arr[itr] != 'string')
-                   return "send input as array of strings";
-
-            if(arr[itr][0] !== 's' && arr[itr][0] !== 'S')
-                     out.push(arr[itr]);
+        return this.removedNamesStartingWithSArray(arr);
+    }
+    
+    removedNamesStartingWithSArray(arr){
+         let out = [];
+         for(let itr =0;itr<arr.length;itr++){
+              if(typeof  arr[itr] !== 'string')
+                 return "send input as array of strings";
+              
+              let str = arr[itr].toLowerCase();
+              if(str[0] !== 's')
+                 out.push(arr[itr]);
         }
-       
         return out;
      }
 }
